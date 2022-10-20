0783053361e6434011ba84fc044cafac3c1acd2c
Art Pau <arturas.study@gmail.com>
2022-10-20 03:12:42 +0200

    frontend & models done

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+const connect = () => {
+    mongoose.connect(process.env.MONGO_URI).then(() => {
+        console.log("Connected to DB");
+    }).catch((err) => {
+        throw err;
+    });
+};
+
+app.listen(8800, () => {
+    connect();
+    console.log("Connected to server")
+});
